fc8c1c9222fbb72be23e10e55ea849ca9b227439
samos <hanyh2004@gmail.com>
2017-12-02 01:42:42 +0800

    modify

--- README.md
+++ README.md
@@ -8,3 +8,51 @@ storage# cosmos
 * super client
 * super node
 * front node
+
+
+## HTTP API
+
+## calcuate_storage_fee
+### try to calcute fee for storage file
+```sh
+URI: /cosmos/calcuate_storage_fee
+Method: PUT
+Args: 
+    filesize: file data
+    expire: expire date
+    qos: quality of service 99%, 99.9%,99.99%,99.999%,99.9999% 
+```
+
+## announce_file
+announce that I have a file
+
+## file_put
+
+### put file to cosmos filesystem
+
+```sh
+URI: /cosmos/file_put
+Method: PUT
+Args: 
+    filename: filename(option)
+    data: file data
+    expire：expire date（0） means never expire(option)
+    fee: 
+```
+
+example:
+
+```sh
+curl http://127.0.0.1:6420/cosmos/file_put
+```
+
+result:
+
+```json
+
+```
+
+文件上传
+
+
+### file_get
--- src/file/file.go
+++ src/file/file.go
@@ -8,24 +8,52 @@ import (
 	"encoding/json"
 	"log"
 	"os"
-	"time"
 	"reflect"
+	"time"
 )
 
+/***
+* 场景：
+* 1,查询文件存在不 get_metainfo_by_hash
+* 2,查询块存在不 
+*
+ */
+
 type MetaInfo struct {
-	Size       float32       `json:"size"`
-	FileName   string        `json:"file_name"`
-	PubKey     []byte        `json:"public_key"`
-	Hash       string        `json:"hash"`
-	Sig        string        `json:"sig"`
-	CreateTime int64         `json:"create_time"`
-	Blocks     map[int]Block `json:"blocks"`
+	//一个文件存多个块
+	Size float64 `json:"size,omitempty"`      //文件总大小
+	Name string  `json:"file_name,omitempty"` //文件名字
+	Hash string  `json:"file_hash,omitempty"` //文件的hash
+	
+	//一个block存放多个文件
+	Sizes   []float32 `json:"sizes,omitempty"`      //多文件的大小
+	Names   []string  `json:"file_names,omitempty"` //多个文件
+	Hashs   []string  `json:"file_hashs,omitempty"` //多个文件的hash
+	Offsets []float32 `json:"offsets,omitempty"`    //偏移
+
+	Blocks      []string `json:"blocks"`       //每一块的hash，多文件存一个块是
+
+	
+	Publisher   string   `json:"publisher"`    //文件上传者的公钥
+	Expire      int      `json:"expire"`       //过期时间
+	Priority    int      `json:"priority"`     //优先级
+	Sig         string   `json:"sig"`          //上传用户的签名
+	CreateTime  int      `json:"create_time"`  //创建时间
+	UpdateTime  int      `json:"update_time,omitempty"`  //更新时间
+	Ver         int      `json:"version,omitempty"`      //版本，只有s
+	BlockLength int      `json:"block_length,omitempty"` //每一块的大小，默认512K
+	Cipher      string   `json:"cipher,omitempty"`       //文件token密文，[方法|密文],比如[aes256|xxxxxxxx]，如果不为空，则blocks数据保密
+	Comment     string   `json:"comment,omitempty"`      //文件注释
+	Tag         string   `json:"tag,omitempty"`          //文件tag
+	Announce    string   `json:"announce,omitempty"`     //最开始接收服务的地址，公钥地址（相当于Tracker)；可以多个，逗号分隔
+
+	//add more
+
 }
 
 type Block struct {
-	PubKey []byte `json:"public_key"`
-	Hash   string `json:"hash"`
-	Data   []byte `json:"Data"`
+	Hash string `json:"hash"`
+	Data []byte `json:"Data"`
 }
 
 func main() {
