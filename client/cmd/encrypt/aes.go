190033516b19739e2b8e5ff8fc91607d7cdc2bb0
zhiyuan2007 <liuben5918@gmail.com>
2018-06-20 10:42:46 +0800

    aes encry

--- client/cmd/encrypt/aes.go
+++ client/cmd/encrypt/aes.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
+
+	"github.com/samoslab/nebula/client/common"
 
 	"github.com/spf13/pflag"
 )
-
-func rand_str(str_size int) string {
-	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	var bytes = make([]byte, str_size)
-	rand.Read(bytes)
-	for i, b := range bytes {
-		bytes[i] = alphanum[b%byte(len(alphanum))]
-	}
-	return string(bytes)
-}
 
 func main() {
 	input := pflag.StringP("input", "i", "", "input file")
@@ -30,9 +15,13 @@ func main() {
 	password := pflag.StringP("password", "p", "", "password")
 	pflag.Parse()
 
-	fmt.Printf("pass %s\n", *password)
 	if *password == "" {
 		fmt.Printf("need password -p\n")
+		pflag.PrintDefaults()
+		return
+	}
+	if len(*password) != 16 {
+		fmt.Printf("password length need 16\n")
 		pflag.PrintDefaults()
 		return
 	}
@@ -51,82 +40,23 @@ func main() {
 		pflag.PrintDefaults()
 		return
 	}
+
+	var err error
+
 	switch *method {
 	case "enc":
-		encryptFile(*input, []byte(*password), *output)
+		err = common.EncryptFile(*input, []byte(*password), *output)
 	case "dec":
-		decryptFile(*input, []byte(*password), *output)
+		err = common.DecryptFile(*input, []byte(*password), *output)
+	default:
+		err = fmt.Errorf("only support enc|dec\n")
 	}
 
-}
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+		return
+	}
 
-func encryptFile(inputfile string, key []byte, outputfile string) {
-	b, err := ioutil.ReadFile(inputfile) //Read the target file
-	if err != nil {
-		fmt.Printf("Unable to open the input file!\n")
-		os.Exit(0)
-	}
-	ciphertext := encrypt(key, b)
-	//fmt.Printf("%x\n", ciphertext)
-	err = ioutil.WriteFile(outputfile, ciphertext, 0644)
-	if err != nil {
-		fmt.Printf("Unable to create encrypted file!\n")
-		os.Exit(0)
-	}
-}
+	fmt.Printf("success\n")
 
-func decryptFile(inputfile string, key []byte, outputfile string) {
-	z, err := ioutil.ReadFile(inputfile)
-	result := decrypt(key, z)
-	//fmt.Printf("Decrypted: %s\n", result)
-	fmt.Printf("Decrypted file was created with file permissions 0644\n")
-	err = ioutil.WriteFile(outputfile, result, 0644)
-	if err != nil {
-		fmt.Printf("Unable to create decrypted file!\n")
-		os.Exit(0)
-	}
-}
-
-func encodeBase64(b []byte) []byte {
-	return []byte(base64.StdEncoding.EncodeToString(b))
-}
-
-func decodeBase64(b []byte) []byte {
-	data, err := base64.StdEncoding.DecodeString(string(b))
-	if err != nil {
-		fmt.Printf("Error: Bad Key!\n")
-		os.Exit(0)
-	}
-	return data
-}
-
-func encrypt(key, text []byte) []byte {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-	b := encodeBase64(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], b)
-	return ciphertext
-}
-func decrypt(key, text []byte) []byte {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-	if len(text) < aes.BlockSize {
-		fmt.Printf("Error!\n")
-		os.Exit(0)
-	}
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-	return decodeBase64(text)
 }
