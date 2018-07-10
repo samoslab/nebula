7aade444d1e029fea6f6a3ce432bb2098e4ea9a2
lijiangt <lijiangt@gmail.com>
2018-07-10 11:24:00 +0800

    client register interface optimize

--- tracker/register/client/pb/sign.go
+++ tracker/register/client/pb/sign.go
@@ -103,6 +103,23 @@ func (self *OrderInfoReq) VerifySign(pubKey *rsa.PublicKey) error {
 	return rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, self.hash(), self.Sign)
 }
 
+func (self *RemoveOrderReq) hash() []byte {
+	hasher := sha256.New()
+	hasher.Write(self.NodeId)
+	hasher.Write(util_bytes.FromUint64(self.Timestamp))
+	hasher.Write(self.OrderId)
+	return hasher.Sum(nil)
+}
+
+func (self *RemoveOrderReq) SignReq(priKey *rsa.PrivateKey) (err error) {
+	self.Sign, err = rsa.SignPKCS1v15(rand.Reader, priKey, crypto.SHA256, self.hash())
+	return
+}
+
+func (self *RemoveOrderReq) VerifySign(pubKey *rsa.PublicKey) error {
+	return rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, self.hash(), self.Sign)
+}
+
 func (self *RechargeAddressReq) hash() []byte {
 	hasher := sha256.New()
 	hasher.Write(self.NodeId)
