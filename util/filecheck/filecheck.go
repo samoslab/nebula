5ae895001ec1d85b240a4cea0cd8fee0bf8c2a07
lijiangt <lijiangt@gmail.com>
2018-12-01 23:07:55 +0800

    test runner

--- util/filecheck/filecheck.go
+++ util/filecheck/filecheck.go
@@ -6,9 +6,17 @@ import (
 	"math/big"
 	"os"
 
-	"github.com/Nik-U/pbc"
 	util_bytes "github.com/samoslab/nebula/util/bytes"
 )
+
+const param_str = `type a
+q 3002478024387064405972144620549462508470407736306576747124945076471270999639488002912145296429925462661426869942567633459180609465273495576145476905067727
+h 4108757660880982117779991133481651099122489506463043933048116394881517164177595631157352416201767211244336
+r 730750818665534535851578973600197997769233793023
+exp2 159
+exp1 116
+sign1 1
+sign0 -1`
 
 func GenMetadata(filepath string, chunkSize uint32) (paramStr string, generator []byte, pubKeyBytes []byte, random []byte, phi [][]byte, er error) {
 	file, err := os.Open(filepath)
@@ -24,9 +32,14 @@ func GenMetadata(filepath string, chunkSize uint32) (paramStr string, generator 
 	}
 	size := (fi.Size() + int64(chunkSize) - 1) / int64(chunkSize)
 	phi = make([][]byte, 0, size)
-	params := pbc.GenerateA(160, 512)
-	paramStr = params.String()
-	pairing := params.NewPairing()
+	// params := pbc.GenerateA(160, 512)
+	// paramStr = params.String()
+	paramStr = param_str
+	pairing, err := NewPairingFromString(param_str)
+	if err != nil {
+		panic(err)
+	}
+	// pairing := params.NewPairing() // TODO fix panic
 	g := pairing.NewG2().Rand()
 	generator = g.Bytes()
 	priKey := pairing.NewZr().Rand()
